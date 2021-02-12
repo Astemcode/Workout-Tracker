8a3ce6b94a0251dea99ab5089014c2a6c4ab8ff5
Joivre Charles <astemcode@gmail.com>
2021-02-12 04:28:17 -0500

    Update server.js
    Added the 'api.js' route to server file

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-require ("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+const db = require('./models');
 
 const app = express();
 
-app.use(logger("dev"));
-
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-app.use(require('./routes/html-routes.js'))
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+});
+app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js'));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+	console.log(`App running on port ${PORT}!`);
+});
